f0fca966aed7a37ddcdd7dab4bece02e00564e3c
ElliotKampsten <elliot.kampsten@elev.ga.ntig.se>
2023-04-01 19:45:26 +0200

    Landingpage nästan klar
    Gjorde klart, footer i mobilläge, fixade desktop läge, gjorde ikoner i gimp och mer.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const links = document.getElementById("links")
 const burgare = document.getElementById("burgare")
-
+const Knapp = document.getElementById("DownLoadButton")
+const KnappSkugga = document.getElementById("DownLoadButtonShadow")
 function LinkToggle(){
     links.classList.toggle("links_active")
     burgare.classList.toggle("burgertoggle")
+    Knapp.classList.toggle("DownloadInvis")
+    KnappSkugga.classList.toggle("DownloadInvis")
     
 }
 
